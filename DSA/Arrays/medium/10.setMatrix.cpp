f4659b62e20405330f3d6043df642c2871ce522d
Shivansh Saxena <i.shivanshhh@gmail.com>
2026-06-03 21:42:16 +0530

    Implement matrix rotation and set zeroes algorithms with driver code

---
+++ DSA/Arrays/medium/10.setMatrix.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <unordered_set>
+    
+using namespace std;
+// first approch is to mark the row and column with -1 and then change all -1 to 0 , time complexity is TC => 0(N*M) + 0(N+M) + 0(N*M) => 0(N*M) and space complexity is SC => 0(1) as we are not using any extra space
+
+class Solution {
+    public : 
+    int markRow(vector <vector <int>> & matrix, int i){
+        int n = matrix[0].size(); // columns
+        for (int j = 0; j < n; j++){
+            if(matrix[i][j] != 0){ 
+                matrix[i][j] = -1;
+            }
+        }
+    }
+    int markCol(vector <vector <int>> & matrix, int j){
+        int m = matrix.size(); // rows 
+        for (int i = 0; i < m; i++){
+            if(matrix[i][j] != 0){
+                matrix[i][j] = -1;
+            }
+        }
+    }
+    int setZeroes(vector <vector <int>> & matrix){
+        int m = matrix.size(); // rows
+        int n = matrix[0].size(); // columns
+        for (int i = 0; i < m; i ++){
+            for (int j = 0; j < n; j++){
+                if (matrix [i][j] == 0){
+                    markRow(matrix , i);  // i => row 
+                    markCol(matrix, j);  // j => column
+                }
+            }
+        }
+        for(int i = 0; i < n ; i++){
+            for (int j = 0; j < m; j++){
+                if (matrix[i][j] == -1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+// second approch is to use extra space to store hash check of row and column then check in matrix if any element is 0 then mark the row and column in hash check or 1 then iterate again in matrix and convert all the marked row and column to 0 , TC => 0(N*M) AND SC => 0(N+M) as we are using two extra hash check for row and column
+
+class Solution1 {
+    public : 
+    int setZeroes1(vector <vector<int>>& matrix){
+        int m = matrix.size(); // rows
+        int n = matrix[0].size(); // columns
+
+        vector <int> row(m,0); // take row size hash which mark when it found any 0 in column 
+        vector <int> col(n,0); // similarly it will make 1 when found 0 in any row
+
+        for (int i = 0 ; i < m ; i ++){
+            for (int j = 0; j < n ; j++){
+                if (matrix[i][j] == 0){
+                    row[i] = 1; // found zero at any position mark it with 
+                    col[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+// third approch to reduce space complexity , instead of marking on hash we will mark inside the matrix 
+
+class Solution2{
+    public :
+    int setZeroes2(vector <vector<int>>&matrix){
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int col0 = 1;
+
+        for (int i = 0; i < m ; i ++){
+            for (int j = 0 ; j < n; j++){
+                if (matrix[i][j] == 0){
+                    // mark the i-th row
+                    matrix[i][0] = 0;
+                    // mark the j-th column
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else 
+                        col0 = 0;
+                    }
+                }
+            }
+        for (int i = 1; i < m ; i++){
+            for (int j = 1; j < n; j++){
+                if (matrix[i][j] != 0 ){
+                    if (matrix[0][i] == 0 || matrix[i][0] == 0){
+                        matrix [i][j] = 0;
+                    } 
+                }
+            }
+        }
+        if (matrix[0][0] == 0){
+            for (int j = 0; j < n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0 == 0){
+            for (int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution2 obj;
+    vector<vector<int>> matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+    obj.setZeroes2(matrix);
+    for (auto row : matrix) {
+        for (auto val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
--- leetcodeeasy.cpp
+++ leetcodeeasy.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
-#include <map>
 #include <vector>
-#include <unordered_set>
-    
+
 using namespace std;
 
+class Solution {
+public:
+    // Function to rotate the matrix 90 degrees clockwise using extra space
+    vector<vector<int>> rotateClockwise(vector<vector<int>>& matrix) {
+        // Get the size of the square matrix
+        int n = matrix.size();
+
+        // Create a new matrix of same size to store rotated result
+        vector<vector<int>> rotated(n, vector<int>(n));
+
+        // Traverse each element of original matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Place the element at its new rotated position
+                rotated[j][n - i - 1] = matrix[i][j];
+            }
+        }
+
+        // Return the rotated matrix
+        return rotated;
+    }
+};
+
+// second approch => first transpose the matrix then reverse each and every row. TC 0(N*N) SC 0(1)
+
+
+// Driver code
+int main() {
+    vector<vector<int>> mat = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    Solution obj;
+    vector<vector<int>> rotated = obj.rotateClockwise(mat);
+
+    // Print the rotated matrix
+    for (auto row : rotated) {
+        for (int val : row) cout << val << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
