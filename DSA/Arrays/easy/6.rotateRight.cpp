4f6681734565d1810de7d0ec04dad42bb89938d9
Shivansh Saxena <i.shivanshhh@gmail.com>
2026-07-21 23:58:55 +0530

    Add right rotation function for arrays and level-order traversal implementation for binary trees

---
+++ DSA/Arrays/easy/6.rotateRight.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+// left rotate an array by k steps 
+
+// firslt do {k = k % n} in case k is greater than n , store first k elements in a temp array shift remaining elements to the left and then put back temp array elements to the end of the original array
+
+// time complexity O(n) and space complexity O(k)
+
+int RightRotate(int arr[] , int n, int k){
+    int temp[k];
+    // stored the last k elements in a temporary array
+    for(int i = n-k; i < n; i++){
+        temp[i-(n-k)] = arr[i];
+    }
+    // shift the remaining elements to the right
+    for(int i = 0 ; i < n-k; i++){
+        arr[i+k] = arr[i];
+    }
+    // copy the elements from the temporary array to the end of the original array
+    for(int i = 0; i < k; i++){
+        arr[i] = temp[i];
+    }
+    cout << "Array after right rotation: ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";  
+    }
+    cout << endl;
+}
+
+// second approch using reversal algorithm TC:- O(n) and SC:- O(1)
+// for left rotation always reverse the first k elements, then reverse the remaining n-k elements and finally reverse the whole array
+// for right rotation always reverse the last k elements, then reverse the remaining n-k elements and finally reverse the whole array
+// use reverse function to reverse the elements in the array only in c++ 
+int main(){
+    int k ; 
+    cin >> k;
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    k = k % n; // in case k is greater than n
+    RightRotate(arr, n, k);
+}
